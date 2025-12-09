0d967d2c2c0a9e968c8457df5dbf2def44330b94
Igor Sinchuk <igormac@Igors-MacBook-Air.local>
2025-12-09 22:01:45 +0200

    homework 4

---
+++ lesson4/jsBasics/task1.js
@@ -0,0 +1,31 @@
+const userNAme = "user name";
+console.log(typeof userNAme);
+console.log(userNAme);
+
+const age = 37;
+console.log(typeof age);
+console.log(age);
+
+let noValue = undefined;
+console.log(typeof undefined);
+console.log(typeof noValue);
+
+let trueOrFalse = Boolean;
+console.log(typeof trueOrFalse);
+console.log(trueOrFalse);
+
+let computer = {cpu: "Intel", gpu: "AMD"};
+console.log(typeof computer);
+console.log(computer);
+
+const hi = function() { console.log("Hello!"); };
+console.log(typeof hi);
+console.log(hi);
+
+const hugeHex = BigInt("0x1fffffffffffff");
+console.log(typeof hugeHex);
+console.log(hugeHex);
+
+const sym1 = Symbol("⌘, ⌘, ⌘");
+console.log(typeof sym1);
+console.log(sym1);
